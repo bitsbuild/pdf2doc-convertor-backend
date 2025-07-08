9ebde86ecd262fe27b03ec8a7f6e135d78710c63
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 22:43:22 +0530

    PDF 2 DOC Functionality Completed, Testing Pending

--- convertor/app/views.py
+++ convertor/app/views.py
@@ -1,9 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from app.models import Pdf
 from app.serializers import PdfSerializer
-from django.conf import settings
-from rest_framework.response import Response
 import os
+from django.conf import settings
+from pdf2docx import Converter
+from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 class PdfViewSet(ModelViewSet):
     queryset = Pdf.objects.all()
@@ -12,3 +13,10 @@ class PdfViewSet(ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+        pdf_path = os.path.join(settings.MEDIA_ROOT,'file.pdf')
+        docx_path_to_be = os.path.join(settings.MEDIA_ROOT,'result.docx')
+        cv = Converter(pdf_path)
+        cv.convert(docx_path_to_be)
+        cv.close()
+        docx_url = request.build_absolute_uri(docx_path_to_be)
+        return Response({"DownloadLink":docx_url},status=HTTP_200_OK)
