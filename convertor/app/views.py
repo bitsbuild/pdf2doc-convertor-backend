11ca9870eab922f857875086710f0b1415feb07c
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 20:34:07 +0530

    Viewset Written

---
+++ convertor/app/views.py
@@ -0,0 +1,6 @@
+from rest_framework.viewsets import ModelViewSet
+from app.models import Pdf
+from app.serializers import PdfSerializer
+class PdfViewSet(ModelViewSet):
+    queryset = Pdf.objects.all()
+    serializer_class = PdfSerializer()
