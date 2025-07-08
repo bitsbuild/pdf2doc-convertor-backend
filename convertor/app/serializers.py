d6c959d233bb6d8c49b38034ad6734d5bccff126
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 20:30:16 +0530

    Serializer Completed

--- convertor/app/serializers.py
+++ convertor/app/serializers.py
@@ -1,3 +1,6 @@
 from rest_framework.serializers import ModelSerializer
+from app.models import Pdf
 class PdfSerializer(ModelSerializer):
-    pass
+    class Meta:
+        model = Pdf
+        fields = '__all__'
