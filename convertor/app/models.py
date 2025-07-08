4fa8ee75207b96501909a86a9373e59e11998efb
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 21:05:26 +0530

    At Any Time Only File Will Be There So File Name Will Be file.pdf For Ease Of Computation

--- convertor/app/models.py
+++ convertor/app/models.py
@@ -1,3 +1,8 @@
 from django.db.models import Model,FileField
+import os
+def file_name_set(instance,filename):
+    extension = filename.split('.')[-1]
+    filename = f"file.{extension}"
+    return filename
 class Pdf(Model):
-    pdf = FileField(upload_to='')
+    pdf = FileField(upload_to=file_name_set)
