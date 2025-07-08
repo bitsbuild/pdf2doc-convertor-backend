76b3098a1c321c7a14419a374107330147fc7539
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 23:42:14 +0530

    Reverting Cleanup

--- convertor/app/urls.py
+++ convertor/app/urls.py
@@ -1,9 +1,5 @@
 from rest_framework.routers import DefaultRouter
-
 from app.views import PdfViewSet
-
 router = DefaultRouter()
-
 router.register(r'pdf',PdfViewSet,basename='pdf')
-
 urlpatterns = router.urls
