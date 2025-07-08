0fa9e9b0910a6f527f9e5801090acf85f182623a
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 23:41:41 +0530

    Making URLs file App Level Readable

--- convertor/app/urls.py
+++ convertor/app/urls.py
@@ -1,5 +1,9 @@
 from rest_framework.routers import DefaultRouter
+
 from app.views import PdfViewSet
+
 router = DefaultRouter()
+
 router.register(r'pdf',PdfViewSet,basename='pdf')
+
 urlpatterns = router.urls
