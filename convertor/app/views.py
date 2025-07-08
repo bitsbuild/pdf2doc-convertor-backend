b2e056cfe5273d5f4354987e0b5c182f8e084b16
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-08 22:21:12 +0530

    Testing Sending Download Link As Of Now For Same File Without Processing

--- convertor/app/views.py
+++ convertor/app/views.py
@@ -1,8 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 from app.models import Pdf
 from app.serializers import PdfSerializer
+from django.conf import settings
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 class PdfViewSet(ModelViewSet):
     queryset = Pdf.objects.all()
     serializer_class = PdfSerializer
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        fileurl = os.path.join(settings.MEDIA_URL,'file.pdf')
+        return Response({
+            "Download-Link":fileurl
+        },status=HTTP_200_OK)
